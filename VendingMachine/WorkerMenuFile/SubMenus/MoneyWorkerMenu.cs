0e2f9d1c00054076965149a82cfdb3dfef565fad
KamilKida <115663490+KamilKida@users.noreply.github.com>
2024-02-20 02:51:57 +0100

    Add files via upload

---
+++ VendingMachine/WorkerMenuFile/SubMenus/MoneyWorkerMenu.cs
@@ -0,0 +1,131 @@
+﻿using System.Data.SqlClient;
+using VendingMachine.SQLcomunication;
+using VendingMachine.DefaultContinueFunctions;
+using VendingMachine.DateObjectsWorkers;
+
+namespace VendingMachine.WorkerMenu.SubMenus
+{
+    internal class MoneyWorkerMenu : DefaultOptions
+    {
+        /// <summary>
+        /// List with one argument representing current money in vending machine.
+        /// </summary>
+        List<decimal> moneyInMachine = ISQLcomunication.GetList(GetProfits, "[Money in Machine]");
+        
+
+        /// <summary>
+        /// This is logic behind worker money menu. Uses switch statements based on user input using: 'MoneyMenuLook()' method.
+        /// </summary>
+        /// <param name="name">Name of worker currently using worker menu.</param>
+        /// <param name="worker">'WorkerObj' object used for send actions to database using: 'SendAction()' method.</param>
+        public void MoneyMenuInterface(string name, WorkerObj worker)
+        {
+            Console.Clear();
+            string workerInput = MoneyMenuLook(name);
+            bool moneyToken = true;
+            bool firstRun = true;
+
+            if(workerInput.ToLower() != "x") {
+                while (moneyToken == true)
+                {
+                    if(firstRun != true) { 
+                    Console.Clear();
+                    workerInput = MoneyMenuLook(name);
+                    }
+                    firstRun = false;
+
+                    switch (workerInput.ToLower())
+                    {
+                        case "s":
+                            Console.Clear();
+                            ShowMoney();
+                            worker.SendAction("Looked at money in vending machine. ");
+                            ToContinue();
+                            break;
+                        case "w":
+                            Console.Clear();
+                            WithdraAllMoney(worker);
+                            ToContinue();
+                            break;
+                        case "x":
+                            moneyToken = false;
+                            break;
+                        default:
+                            DefaultChoice(workerInput);
+                            break;
+                    }
+                }
+            }
+
+        }
+        /// <summary>
+        /// Has two functions. One: determines worker money menu look and asks user for input.
+        /// </summary>
+        /// <returns>Input of client used in: 'MoneyMenuInterface()' method.</returns>
+        public string MoneyMenuLook(string name)
+        {
+            string optins = $"s: Show money in vending machine. \n" +
+                            $"w: Withdraw all money in vending machine.";
+
+            DefaultMenu("MONEY OPTIONS", name, optins, false,true);
+            string workerInput = Console.ReadLine();
+            return workerInput;
+        }
+
+        /// <summary>
+        /// Public static method used to create list: 'moneyInMachine'.
+        /// </summary>
+
+        public static decimal GetProfits(SqlDataReader reader)
+        {
+            decimal profits = reader.GetDecimal(1);
+
+
+            return   Math.Round(profits, 2);
+        }
+
+        /// <summary>
+        /// Shows money in vending machine.
+        /// </summary>
+        private void ShowMoney()
+        {
+            Console.WriteLine($"There is currently: {moneyInMachine[0]} $ in vending machine.");
+            Console.WriteLine(" ");
+
+        }
+
+        /// <summary>
+        /// Allows worker to withdraw all money from vending machine if there is any.
+        /// </summary>
+        /// <param name="worker">'WorkerObj' object used for send actions to database using: 'SendAction()' method.</param>
+        private void WithdraAllMoney(WorkerObj worker)
+        {
+            if(moneyInMachine[0] == 0)
+            {
+                Console.WriteLine("You can not withdraw money from vending machine because there is 0,00 $ insde. ");
+                Console.WriteLine(" ");
+                
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    YesNo($"Would you like to withdraw: {moneyInMachine[0]} $ from vending machine ");
+                    string choice = Console.ReadLine();
+
+                    if (choice.ToLower() == "y") {
+                        Console.Clear();
+                        worker.SendAction($"Withdraw all money: {moneyInMachine[0]} $ from vending machine: ");
+                        ISQLcomunication.SendDate("UPDATE [Money In Machine] SET [Current Money In Machine] = 0 WHERE ID = 1");
+                        Console.WriteLine($"You have withdraw: {moneyInMachine[0]} $ from vending machine. \n");
+                        moneyInMachine[0] = 0;
+                        break;
+                    }
+                    else if (choice.ToLower() == "n") { Console.Clear(); Console.WriteLine("Moeny was not taken. \n"); break; }
+                    else { Console.Clear(); DefaultChoice(choice); continue; }
+                }
+            }
+        }
+    }
+}
